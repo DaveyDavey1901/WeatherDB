8f6a8c8b8f456befa60e1e24f31a9a3726bd8a6a
David Davies <David_Davies@mail.com>
2021-11-28 14:51:43 +0000

    manged to get the required city from form input

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ var citySrchBtn = document.querySelector("#city-srch-btn");
 var cityNameEl = document.querySelector("#city-name");
 var cityArr = [];
 var apiKey = "aded95d03c66a91f2fd2f8899e03ddf9";
-var city = "london"
 
 
+
+citySrchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  var city = citySrchInput.value;
   var currentForcastApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  
-     fetch(currentForcastApi)
-     .then(function (response) {
-       return response.json();
-     })
-     .then(function (data) {
-       console.log(data);
-       
-     });
+
+  fetch(currentForcastApi)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
